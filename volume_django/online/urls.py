346a652681c2f57ae5c83e498b4132503a5b1085
Joel Robert-Nicoud <joerober@c2r8s10.42lausanne.ch>
2024-05-28 15:06:31 +0200

    added online to frontend

--- volume_django/online/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('room/', views.CreateRoom, name='create-room'),
-    path('test/', views.test, name='test'),
-    # path('api/<str:room_name>/', views.message_list),
-    # path('api/<str:room_name>/<str:username>/', views.user_message_list),
-    ]
