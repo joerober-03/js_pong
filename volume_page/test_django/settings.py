1fa9b2a88139c922ddb33ff3f3e59b9e4fc43da8
Joel Robert-Nicoud <joerober@c2r8s10.42lausanne.ch>
2024-05-29 11:29:52 +0200

    few fixes (networking)

--- volume_page/test_django/settings.py
+++ volume_page/test_django/settings.py
@@ -118,11 +118,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "home_page/static",
-    "/var/www/static/",
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
